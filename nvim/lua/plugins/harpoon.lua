cd91e14bb714f97b93c00dc0a7e8f5677ad52e28
masfuulaji <masfuulaji@gmail.com>
2023-12-06 15:21:55 +0700

    neofetch theme & nvim harpoon

---
+++ nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "ThePrimeagen/harpoon",
+    lazy = false,
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = true,
+    keys = {
+      { "<leader>a", "<cmd>lua require('harpoon.mark').add_file()<cr>" },
+      { "<C-e>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>" },
+      { "<leader>1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>" },
+      { "<leader>2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>" },
+      { "<leader>3", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>" },
+      { "<leader>4", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>" },
+    },
+  },
+}
